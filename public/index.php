eaedd93e7a659556aaf32e3a0bc1b3508f720385
Samirthini <sami050699@gmail.com>
2026-06-12 17:13:47 +0530

    Fix test_connection.php 500 parse error

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+declare(strict_types=1);
 echo "Hello World";
 exit;
-declare(strict_types=1);
 
 use ProEnroll\Api\Config;
 use ProEnroll\Api\Http\Request;
